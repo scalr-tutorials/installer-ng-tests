5266226caaed89154acdb979d818786ea4a44955
Thomas Orozco <thomas@orozco.fr>
2014-05-16 19:23:43 +0200

    Add TZ test

--- test.sh
+++ test.sh
@@ -15,6 +15,8 @@ set -o nounset
 
 szradm=$(PATH=/usr/local/bin:/usr/bin which szradm)
 
+# Ensure that /usr/local/bin is on our path
+PATH=/usr/local/bin:$PATH
 
 # Install pgrep
 
@@ -31,6 +33,13 @@ install_pgrep () {
 }
 command -v pgrep 2>&1 > /dev/null || install_pgrep
 
+install_jq () {
+  jq_path=/usr/local/bin/jq
+  curl --location --fail "http://stedolan.github.io/jq/download/linux64/jq" > $jq_path
+  chmod +x $jq_path
+}
+command -v jq 2>&1 > /dev/null || install_jq
+
 # Are we done installing?
 pgrep -lf "python install.py" && echo "Install in progress" && exit 0
 
@@ -43,8 +52,19 @@ report_error () {
 echo "Testing for errors"
 grep -PB$TEST_REPORT_LINES 'ERROR|FATAL' $INSTALLER_LOG_FILE && report_error  # There should be no error in the install log
 
+
 echo "Testing for proper install termination"
 grep 'Congratulations' $INSTALLER_LOG_FILE || report_error  # The install should be done
+
+
+echo "Testing for timezone consistency"
+# Note: this may fail if we test at the second where we change hours... Probably not common enough to care: it's just a test script
+mysql_hour=$(mysql --user=root --password=$(jq --raw-output ".mysql.server_root_password" "/root/solo.json") --skip-column-names --batch --execute="SELECT HOUR(NOW())")
+php_hour=$(php -r 'echo date("H") . "\n";')
+
+if [ "$mysql_hour" != "$php_hour" ]; then
+  report_error
+fi
 
 if [ -n $SCALR_START_TESTS ]; then
   $szradm --fire-event=$START_TESTS_EVENT
