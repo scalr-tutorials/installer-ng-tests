fbf193fdbdaf698b7f9f9616ef25c165613fe280
Thomas Orozco <thomas@orozco.fr>
2014-08-28 13:58:34 +0200

    Add an actual test scenario

--- user.py
+++ user.py
@@ -1,6 +1,45 @@
 #!/usr/bin/env python
-import scalr_client
+import json
+import base64
+
+from scalr_client import session
+
+
+INSTALLER_CONFIG_PATH = "/root/solo.json"
+
+TEST_USER_NAME = "Test User"
+TEST_USER_EMAIL = "user@scalr.com"
+TEST_USER_PASSWORD_ENTROPY_BYTES = 24
+
+
+def make_test_user_password(n_entropy):
+    with open('/dev/urandom') as f:
+        return base64.b64encode(f.read(n_entropy))
 
 
 if __name__ == "__main__":
-    print "OK"
+    with open(INSTALLER_CONFIG_PATH) as f:
+        installer_config = json.load(f)
+
+    base_url = installer_config["scalr"]["endpoint"]["host"]
+
+    # Find admin creds
+    admin = installer_config["scalr"]["admin"]
+    admin_username = admin["username"]
+    admin_password = admin["password"]
+
+    # Login as admin
+    adm_session = session.ScalrSession(base_url=base_url)
+    adm_session.login(admin_username, admin_password)
+
+    # Create a new user
+    test_user_password = make_test_user_password(TEST_USER_PASSWORD_ENTROPY_BYTES)
+    adm_session.create_account(TEST_USER_NAME, TEST_USER_EMAIL, test_user_password)
+
+    # Login as the user
+    user_session = sessioN.ScalrSession(base_url=base_url)
+    user_session.login(TEST_USER_EMAIL, test_user_password)
+
+    # Do something
+    print user_session.get_ec2_cloud_params()
+
