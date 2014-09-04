8d1510f71d8eab03a510ed8f4882e41d2496b621
Thomas Orozco <thomas@orozco.fr>
2014-09-04 18:54:02 +0200

    Log an error if we can't find the branch
    Don't use --exit-code, which isn't there in older versions of git

--- report_github.sh
+++ report_github.sh
@@ -18,7 +18,13 @@ report_github_ci_status () {
 
   # Now find out what the hash is, by asking the remote.
   # We can't get it locally, because Scalr may not be deployed at this point yet.
-  local branch_ref=$(git ls-remote --heads --exit-code "${INSTALLER_REPOSITORY_URL}" "${INSTALLER_BRANCH}")
+  local branch_ref=$(git ls-remote --heads "${INSTALLER_REPOSITORY_URL}" "${INSTALLER_BRANCH}")
+
+  if [ -z "$branch_ref" ]; then
+    echo "Branch ${INSTALLER_BRANCH} does not seem to exist."
+    exit 1
+  fi
+
   local branch_sha=$(echo "${branch_ref}" | cut -c-40)  # A commit hash is 40 chars long
   local payload="{\"state\": \"${status}\", \"context\": \"${SCALR_FARM_ROLE_ALIAS}-${SCALR_FARM_ID}\"}"
 
