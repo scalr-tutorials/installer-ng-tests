a311a20d6df9c0a118b89e183aa495d16eb47b7b
Thomas Orozco <thomas@orozco.fr>
2014-10-27 12:17:17 +0100

    Provide useful login information in the setup log

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ HERE=$(cd "${REL_HERE}"; pwd)  # Get an absolute path
 source "${HERE}/constants.sh"
 
 ### Actual script
+
+# First, sync shared Roles
+
 SCALR_PATH="/opt/scalr/current"
 ID_FILE="${SCALR_PATH}/app/etc/id"
 SYNC_SCRIPT="${SCALR_PATH}/app/tools/sync_shared_roles.php"
@@ -17,3 +20,9 @@ cp -p "${ID_FILE}" "${ID_FILE}.bak"
 echo "${SCALR_SYNC_ID}" > "${ID_FILE}"
 php "${SYNC_SCRIPT}"
 mv "${ID_FILE}.bak" "${ID_FILE}"
+
+
+# Second, get us the admin password and log it
+echo "Login URL     : $(jq --raw-output ".scalr.endpoint.host" "/root/solo.json")"
+echo "Admin username: $(jq --raw-output ".scalr.admin.username" "/root/solo.json")"
+echo "Admin password: $(jq --raw-output ".scalr.admin.password" "/root/solo.json")"
